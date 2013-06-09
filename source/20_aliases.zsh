b1dc695a6c746a8167688208779c6a97a66b0329
ejoubaud <emmanuel.joubaud@gmail.com>
2013-06-09 12:00:00 +1000

    Zeus check was being too loud

--- source/20_aliases.zsh
+++ source/20_aliases.zsh
@@ -9,5 +9,5 @@ alias gbw="git build-without-master"
 alias pr="touch ~/.pow/restart.txt"
 
 # Bundler (Undo harmful from oh-my-zsh)
-alias zeus && unalias zeus
+alias zeus > /dev/null && unalias zeus
 
