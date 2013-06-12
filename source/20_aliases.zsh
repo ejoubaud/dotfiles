f59eaa4a2910e5dd4d4b705ab0e59a0bfcf30a86
ejoubaud <emmanuel.joubaud@gmail.com>
2013-06-12 18:56:39 +1000

    Add rake db:test:clone to alias rdm

--- source/20_aliases.zsh
+++ source/20_aliases.zsh
@@ -12,3 +12,6 @@ alias pr="touch ~/.pow/restart.txt"
 # Bundler (Undo harmful from oh-my-zsh)
 alias zeus > /dev/null && unalias zeus
 
+# Rails
+alias rdm='rake db:migrate && rake db:test:prepare'
+
