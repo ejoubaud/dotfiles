0214153970041ab59e4ec8ec557ec67eec0c2527
ejoubaud <emmanuel.joubaud@gmail.com>
2013-12-10 12:12:43 +1100

    Update zeus aliases

--- source/20_aliases.zsh
+++ source/20_aliases.zsh
@@ -14,3 +14,10 @@ alias zeus > /dev/null && unalias zeus
 
 # Rails
 alias rdm='rake db:migrate && rake db:test:prepare'
+
+# Zeus
+alias zr='zeus rspec'
+alias zc='zeus console'
+alias zs='zeus start'
+alias z='zeus'
+
