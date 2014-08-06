df444b4f81d8836c6eac5a71a14858466f76ae9a
ejoubaud <emmanuel.joubaud@gmail.com>
2014-08-06 11:37:37 +0200

    Add a sourcing file for Envato-specific config

---
+++ source/90_envato.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# For envato's Boxen
+[[ -f /opt/boxen/env.sh ]] && source /opt/boxen/env.sh
+
+# --- Aliases ---
+
+# Start an upgraded rails on master
+alias rs="git checkout master && git smart-pull && (bundle install --local) && rake db:migrate && bundle exec foreman start web"
+
+
