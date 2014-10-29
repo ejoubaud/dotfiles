c7c2529eb76d896b0fc3ac79d92c54deea586a9a
ejoubaud <emmanuel.joubaud@gmail.com>
2014-10-29 21:11:00 +0100

    Improved sr alias to start Rails

--- source/90_envato.zsh
+++ source/90_envato.zsh
@@ -6,7 +6,8 @@
 # --- Aliases ---
 
 # Start an upgraded rails on master
-alias sr="git checkout master && git smart-pull && (bundle install --local) && rake db:migrate && bundle exec foreman start web"
+alias srm="git checkout master && git smart-pull && (bundle install --local) && bundle exec spring binstub --all && bin/rake db:migrate && bundle exec foreman start web"
+alias sr="git checkout master && git smart-pull && (bundle install --local) && bin/rake db:migrate && bundle exec foreman start web"
 
 # Needed for AWS auto-scaling
 export AWS_AUTO_SCALING_HOME="/opt/boxen/homebrew/Cellar/auto-scaling/1.0.61.6/libexec"
