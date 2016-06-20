ac65962622b6e1b5054d7ec20bd499b460b5a710
ejoubaud <emmanuel.joubaud@gmail.com>
2016-06-20 15:11:11 +0200

    Remove unused aliases (annoying srm...)

--- source/90_envato.zsh
+++ source/90_envato.zsh
@@ -3,12 +3,6 @@
 # For envato's Boxen
 [[ -f /opt/boxen/env.sh ]] && source /opt/boxen/env.sh
 
-# --- Aliases ---
-
-# Start an upgraded rails on master
-alias srm="git checkout master && git smart-pull && (bundle install --local) && bundle exec spring binstub --all && bin/rake db:migrate && bundle exec foreman start web"
-alias sr="git checkout master && git smart-pull && (bundle install --local) && bin/rake db:migrate && bundle exec foreman start web"
-
 # Needed for AWS auto-scaling
 export AWS_AUTO_SCALING_HOME="/opt/boxen/homebrew/Cellar/auto-scaling/1.0.61.6/libexec"
 export JAVA_HOME="`/usr/libexec/java_home`"
