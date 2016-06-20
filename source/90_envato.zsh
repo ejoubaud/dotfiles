ca2f368d5c982cf61980c63c531b7598e6ddd576
ejoubaud <emmanuel.joubaud@gmail.com>
2016-06-20 15:11:24 +0200

    Remove outdated auto-scaling requirements

--- source/90_envato.zsh
+++ source/90_envato.zsh
@@ -2,7 +2,3 @@
 
 # For envato's Boxen
 [[ -f /opt/boxen/env.sh ]] && source /opt/boxen/env.sh
-
-# Needed for AWS auto-scaling
-export AWS_AUTO_SCALING_HOME="/opt/boxen/homebrew/Cellar/auto-scaling/1.0.61.6/libexec"
-export JAVA_HOME="`/usr/libexec/java_home`"
