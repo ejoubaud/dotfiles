9cf14fb1fcf3b345a2f646d0bb0acab7ef672daa
ejoubaud <emmanuel.joubaud@gmail.com>
2018-05-11 17:47:38 +0200

    Add disco alias

--- source/90_envato.zsh
+++ source/90_envato.zsh
@@ -2,3 +2,6 @@
 
 # For envato's Boxen
 [[ -f /opt/boxen/env.sh ]] && source /opt/boxen/env.sh
+
+# For disco gem, so we don't have to install it everywhere
+alias disco="RBENV_VERSION=2.3.3 aws-vault exec discovery -- disco"
