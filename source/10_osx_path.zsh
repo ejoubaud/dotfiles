55431010263e70522e5bd5a01a9abc54dabc5273
ejoubaud <emmanuel.joubaud@gmail.com>
2019-05-10 15:59:44 +0200

    Add rbenv and nodenv paths

--- source/10_osx_path.zsh
+++ source/10_osx_path.zsh
@@ -3,10 +3,13 @@
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
-# node
-path+=(/Users/ejoubaud/node_modules/.bin /usr/local/share/npm/bin)
-
 # go
 export GOPATH=~/.go
 export GOBIN=$GOPATH/bin
 path+=($GOBIN)
+
+# rbenv
+export PATH="$HOME/.rbenv/shims:$PATH"
+
+#nodenv
+export PATH="$HOME/.nodenv/bin:$PATH"
