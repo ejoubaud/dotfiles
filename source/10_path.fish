4bba33164034e45a76734c6c2d882ebe520b9443
ejoubaud <emmanuel.joubaud@gmail.com>
2019-12-09 12:27:58 +0100

    Fix anyenv paths

--- source/10_path.fish
+++ source/10_path.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
 # anyenv
-if type anyenv > /dev/null
-  set -Ux fish_user_paths $HOME/.anyenv/bin $fish_user_paths
+if type -q anyenv
+  status --is-interactive; and source (anyenv init -|psub)
 end
