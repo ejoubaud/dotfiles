f9338bec76021eb4ac5bba4fc2c350f1abf1b408
ejoubaud <emmanuel.joubaud@gmail.com>
2019-12-09 12:41:38 +0100

    Add ggpush alias

--- source/20_aliases.fish
+++ source/20_aliases.fish
@@ -7,6 +7,14 @@ alias gbw="git build-without-master"
 alias gsp="git smart-pull"
 alias gst="git status"
 
+function current_branch
+  set ref (git symbolic-ref HEAD 2> /dev/null); or \
+  set ref (git rev-parse --short HEAD 2> /dev/null); or return
+  echo $ref | sed s-refs/heads/--
+end
+
+alias ggpush='git push origin (current_branch)'
+
 # Bundler (Undo harmful from oh-my-zsh)
 alias zeus > /dev/null && unalias zeus
 
