fbf957aed2292930c98b8d4e38665e1286be396b
ejoubaud <emmanuel.joubaud@gmail.com>
2020-01-07 00:06:29 +0100

    Add zsh-autosuggestions

--- init/10_init_zsh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Ensure oh-my-zsh is installed
-[[ -d $HOME/.oh-my-zsh ]] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Setup zsh as the default shell
-[[ $SHELL == */zsh ]] || sudo chsh -s /bin/zsh
---
+++ init/20_init_zsh.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Ensure oh-my-zsh is installed
+[[ -d $HOME/.oh-my-zsh ]] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Add zsh-autosuggestion
+CUSTOM_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+AUTOSUGGESTION_DIR="$CUSTOM_DIR/plugins/zsh-autosuggestions"
+[[ -d "$AUTOSUGGESTION_DIR" ]] || git clone https://github.com/zsh-users/zsh-autosuggestions "$AUTOSUGGESTION_DIR"
+
+# Setup zsh as the default shell
+[[ $SHELL == */zsh ]] || sudo chsh -s /bin/zsh
+
