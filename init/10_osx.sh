e2fe6ae65c3d3da117f6da08d1b679a6d0935073
ejoubaud <emmanuel@go-electra.com>
2023-04-17 22:30:45 +0200

    Add lazygit and ag to brew installs

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -3,7 +3,7 @@
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
-RECIPES=(git tree sl ctags git-delta)
+RECIPES=(git tree sl ctags ag lazygit git-delta)
 
 # Some tools look for XCode, even though they don't need it.
 # https://github.com/joyent/node/issues/3681
