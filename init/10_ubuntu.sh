73e61e5fd83cbeb85f3372b6b493339077531344
Emmanuel Joubaud <emmanuel.joubaud@gmail.com>
2023-09-17 17:25:31 +0200

    Make it work with Linux Mint

--- init/10_ubuntu.sh
+++ init/10_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Ubuntu-only stuff. Abort if not Ubuntu.
-[[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || return 1
+# Ubuntu-only stuff. Abort if not Linux.
+[[ "$(cat /etc/issue 2> /dev/null)" =~ Linux ]] || return 1
 
 # Nothing for now, this is just a placeholder
