84ab3caec5db1d3350caa851020278c807002954
Emmanuel Joubaud <emmanuel.joubaud@gmail.com>
2023-09-19 22:01:19 +0200

    Install fixes

--- init/05_brew.sh
+++ init/05_brew.sh
@@ -2,7 +2,7 @@
 
 # Start by installing homebrew, whether on MacOS or Linux
 
-RECIPES=(git tmux tree sl ctags ag lazygit git-delta asdf gh vim fzf)
+RECIPES=(git tmux tree sl ctags ag lazygit git-delta asdf gh   vim fzf)
 
 # Install Homebrew
 if [[ ! "$(type -P brew)" ]]; then
