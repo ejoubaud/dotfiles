41c847ff4bbda68d07c2d1ad6d84ffa37a789304
ejoubaud <emmanuel.joubaud@gmail.com>
2023-09-19 23:41:24 +0200

    Install xsel for tmux-yank on Linux

--- init/10_ubuntu.sh
+++ init/10_ubuntu.sh
@@ -3,4 +3,7 @@
 # Ubuntu-only stuff. Abort if not Linux.
 [[ "$(cat /etc/issue 2> /dev/null)" =~ Linux ]] || return 1
 
+# requirement for tmux-yank
+sudo apt-get install xsel
+
 # Nothing for now, this is just a placeholder
